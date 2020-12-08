de9ba9d48d0784a1785c2fbf597409589fb70b96
la raw <lraw0819@my.uri.edu>
2020-12-07 20:46:55 -0500

    Build command for both Linux and Mac Project_Strategy File included

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-g++ -Wall main.cpp gl_frontEnd.cpp utils.cpp -lGL -lglut -lpthread -o main
-
+# Linux compilation 
+g++ -Wall -w main.cpp gl_frontEnd.cpp utils.cpp -lGL -lglut -lpthread -o main
+ 
+# Mac OS compilation
+#clang++ -Wall -Wno-deprecated-declarations -std=c++17 -stdlib=libc++ gl_frontEnd.cpp main.cpp utils.cpp -framework OpenGL -framework GLUT -lpthread -o traveler
