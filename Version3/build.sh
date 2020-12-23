2f75bdc1ea366c10db56149fd15b09feff6e5bad
la raw <lraw0819@my.uri.edu>
2020-12-23 02:32:36 -0500

    v..cpp to V...cpp

--- Version3/build.sh
+++ Version3/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-g++ -Wall -w version_3.cpp gl_frontEnd.cpp utils.cpp -lGL -lglut -lpthread -o version_3
+g++ -Wall -w Version_3.cpp gl_frontEnd.cpp utils.cpp -lGL -lglut -lpthread -o version_3
